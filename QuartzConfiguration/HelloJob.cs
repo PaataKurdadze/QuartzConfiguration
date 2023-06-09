bb724cd6776c2cd087ff508422e2d7bcaf77ccdd
Paata Kurdadze <paata2016@gmail.com>
2023-06-09 13:17:39 +0400

    Add project files.

---
+++ QuartzConfiguration/HelloJob.cs
@@ -0,0 +1,17 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartzConfiguration
+{
+    public class HelloJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            Console.Out.WriteLineAsync($"Executing background job => {DateTime.Now}");
+        }
+    }
+}
---
+++ QuartzConfiguration/Program.cs
@@ -0,0 +1,18 @@
+﻿using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartzConfiguration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var scheduler = new StdSchedulerFactory().GetScheduler();
+            scheduler.Start();
+        }
+    }
+}
